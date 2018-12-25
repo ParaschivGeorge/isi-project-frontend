fc3abc381f3a36dff7fa5d65ec8ed09496bd0daf
ParaschivGeorge <georgegmtt@gmail.com>
2018-12-25 12:07:38 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MaterialModule } from './material/material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ErrorPageComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    HttpClientModule,
+    HttpModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://isi-project-backend.herokuapp.com/'
 };
