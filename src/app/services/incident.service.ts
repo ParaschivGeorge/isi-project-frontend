c57d97f8126fe2366c8d7e1be454a16de478e96e
ParaschivGeorge <georgegmtt@gmail.com>
2018-12-28 11:24:14 +0200

    implemented incident service

--- src/app/services/incident.service.ts
+++ src/app/services/incident.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IncidentStatus } from '../models/enums/incident-status.enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IncidentService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getIncidents(): Observable<any> {
+    return this._http.get<any>(environment.apiUrl + '/incident/search/findAllByOrderByTimeDesc');
+  }
+
+  getIncidentsByStatus(status: IncidentStatus): Observable<any> {
+    const queryParam = new HttpParams().set('status', status.toString());
+
+    return this._http.get<any>(environment.apiUrl + '/incident/search/findAllByStatusOrderByTimeDesc', {params : queryParam});
+  }
+
+  updateIncidentStatus(id: number, status: IncidentStatus): Observable<any> {
+    let queryParam = new HttpParams().set('status', status.toString());
+    queryParam = queryParam.append('id', id.toString());
+
+    return this._http.get<any>(environment.apiUrl + '/incident/search/setStatus', {params : queryParam});
+  }
+
+  createIncident(description: string): Observable<any> {
+    let queryParam = new HttpParams().set('description', description);
+    queryParam = queryParam.append('email', localStorage.getItem('email'));
+
+    return this._http.request<any>('post', environment.apiUrl + '/incident/search/setStatus', {params : queryParam});
+  }
 }
