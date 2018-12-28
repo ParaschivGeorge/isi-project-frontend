193fe4ca9a3dae61bb0caa52b30c261580a64109
ParaschivGeorge <georgegmtt@gmail.com>
2018-12-28 11:01:31 +0200

    added incident interface

--- src/app/models/incident.ts
+++ src/app/models/incident.ts
@@ -1,2 +1,9 @@
+import { IncidentStatus } from './enums/incident-status.enum';
+
 export interface Incident {
+    id: number;
+    time: string;
+    status: IncidentStatus;
+    description: string;
+    username: string;
 }
