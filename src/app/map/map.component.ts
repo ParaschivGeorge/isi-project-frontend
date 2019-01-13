aef8a0ee40eb6aa24d22514d45902c7361d0cee4
ParaschivGeorge <georgegmtt@gmail.com>
2019-01-13 23:45:15 +0200

    linked arcgis map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,66 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { EsriModuleProvider } from 'angular-esri-components';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent {
+  screenHeight = 1000;
+  url = 'https://map-isi-project.firebaseapp.com';
 
-  title = 'app';
+  @HostListener('window:resize', ['$event'])
+  onResize(event?) {
+    this.screenHeight = window.innerHeight;
+    // console.log(this.screenHeight);
+ }
 
-  mapProperties: __esri.MapProperties = {
-    basemap: 'dark-gray'
-  };
-  mapViewProperties: __esri.MapViewProperties = {
-    center: [-118, 34.5],
-    zoom: 8
-  };
-  map: __esri.Map;
-  mapView: __esri.MapView;
-
-  ngOnInit() {
-  }
-
-  constructor(private moduleProvider: EsriModuleProvider) { }
-
-  onMapInit(mapInfo: {map: __esri.Map, mapView: __esri.MapView}) {
-    this.map = mapInfo.map;
-    this.mapView = mapInfo.mapView;
-
-    // add a layer with sublayers to map
-    this.moduleProvider
-      .require(['esri/layers/MapImageLayer'])
-      .then(
-        ([MapImageLayer]) => {
-          const layer = new MapImageLayer({
-            url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer',
-            sublayers: [
-              {
-                id: 3,
-                title: 'States',
-                visible: false
-              },
-              {
-                id: 2,
-                title: 'Railroads',
-                visible: true
-              },
-              {
-                id: 1,
-                title: 'Highways',
-                visible: true
-              },
-              {
-                id: 0,
-                title: 'Cities',
-                visible: true
-              }
-            ]
-          });
-
-          this.map.layers.add(layer);
-        });
-  }
+  constructor() {
+    this.onResize();
+   }
 }
